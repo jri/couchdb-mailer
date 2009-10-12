71d9eda1e30985e2be04a844c3adfa1ade96e2dd
Jörg Richter <jri@deepamehta.de>
2009-10-12 02:25:13 +0200

    Initial commit. Rudimentary functionality.

---
+++ README.md
@@ -0,0 +1,67 @@
+
+CouchDB Mailer Extension
+========================
+
+CouchDB Mailer Extension allows your CouchDB applications to send emails.
+
+
+Build from Source
+-----------------
+
+1.  Get Git repository:
+        git clone git://github.com/jri/couchdb-mailer.git
+    A directory `couchdb-mailer` will be created
+2.  Compile and build:
+        cd couchdb-mailer
+        ant
+    The file `dist/couchdb-mailer-0.1-SNAPSHOT.jar` will be created
+
+
+Download Binary
+---------------
+
+<http://cloud.github.com/downloads/jri/couchdb-mailer/couchdb-mailer-0.1-SNAPSHOT.jar>
+
+
+Installation
+------------
+
+1.  Setup Java libraries:
+
+    1.1 Put `couchdb-mailer-0.1-SNAPSHOT.jar` to a directory where you store java libraries  
+        *IMPORTANT*: this directory must be readable by the user which runs the couchdb process (usually user `couchdb`).
+
+    1.2 Put the following 3rd party libraries to the same directory:
+            mail-1.4.2.jar
+            activation-1.1.1.jar
+            json-lib-2.3-jdk13.jar
+            commons-beanutils-core-1.8.0.jar
+            commons-lang-2.4.jar
+            commons-logging-1.1.1.jar
+            ezmorph-1.0.6.jar
+            commons-collections-3.2.1.jar
+
+2.  Configure CouchDB: add lines to /etc/couchdb/local.ini
+
+        [external]
+        mailer=/usr/bin/java -server -jar /path/to/couchdb-mailer-0.1-SNAPSHOT.jar
+
+        [httpd_db_handlers]
+        _mailer = {couch_httpd_external, handle_external_req, <<"mailer">>}
+
+3.  Restart CouchDB
+
+
+Usage
+-----
+
+From your application send a POST request to
+
+    http://www.your-couchdb-host.com/your-couchdb/_mailer
+
+Put the mail content (recipients, subject ...) in the POST request's body.
+
+
+------------
+Jörg Richter  
+Oct 12, 2009
---
+++ src/de/deepamehta/couchdb/mailer/Main.java
@@ -0,0 +1,123 @@
+package de.deepamehta.couchdb.mailer;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+
+
+public final class Main {
+
+	private static final String TEXT_NO_SUBJECT = "<No Subject>";
+
+    private static BufferedReader reader;
+
+    public static void main(final String[] args) {
+
+        System.err.println("### initializing couchdb-mailer extension");
+        reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            try {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                // System.err.println("### got line: " + line);
+                process(line);
+                respond();
+            } catch (IOException e) {
+                throw new RuntimeException("### can't read line", e);
+            }
+        }
+        System.err.println("### exiting couchdb-mailer");
+    }
+
+    private static void process(String line) {
+        JSONObject request = JSONObject.fromObject(line);
+        JSONObject rcpts = JSONObject.fromObject(request.get("body"));
+        List recipients = new ArrayList();
+        Iterator i = rcpts.keys();
+        while (i.hasNext()) {
+            String rcpt = (String) i.next();
+            recipients.add(new Recipient(rcpts.getString(rcpt), rcpt, Message.RecipientType.TO));
+        }
+        //
+        sendMail("jri@deepamehta.de", recipients, "Test", "Text\n2ndline");
+    }
+
+    private static void respond() {
+        System.out.println("{\"body\": \"mail sent sucessfully\"}");
+    }
+
+    /*** JavaMail Part ***/
+
+	public static void sendMail(String from, List recipients, String subject, String body) {
+		try {
+			MimeMessage msg = new MimeMessage(getMailSession());
+			msg.setFrom(new InternetAddress(from));
+			//
+			Iterator i = recipients.iterator();
+            while (i.hasNext()) {
+                Recipient recipient = (Recipient) i.next();
+                Address address = new InternetAddress(recipient.address, recipient.personal);
+    			msg.addRecipient(recipient.type, address);
+            }
+			//
+			if (subject == null || subject.equals("")) {
+				subject = TEXT_NO_SUBJECT;
+			}
+			msg.setSubject(subject);
+			msg.setText(body, "UTF-8");
+			msg.setSentDate(new Date());
+			//
+			Transport.send(msg);
+		} catch (Throwable e) {
+            System.err.println("### error while sending mail: " + e);
+		}
+	}
+
+	private static Session getMailSession() {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "localhost");
+		Session session = Session.getDefaultInstance(props);	// ### authenticator=null
+		session.setDebug(true);		                            // ###
+		return session;
+	}
+
+	// Inner Class
+
+	private static class Recipient {
+
+		String address;
+		String personal;
+		Message.RecipientType type;
+
+		Recipient(String address, String personal, Message.RecipientType type) {
+			this.address = address;
+			this.personal = personal;
+			this.type = type;
+		}
+
+		public boolean equals(Object o) {
+			Recipient r = (Recipient) o;
+			return r.type == type && r.address.equals(address);
+		}
+	}
+}
